46a5a75e2b6a45225993c4a5c71480556d288d97
Mpavanigithub <114507128+Mpavanigithub@users.noreply.github.com>
2022-11-10 17:01:52 +0530

    Add files via upload

---
+++ EjbClientEx/src/com/ejb/calc/CalcBeanRemote.java
@@ -0,0 +1,13 @@
+package com.ejb.calc;
+
+import javax.ejb.Remote;
+
+@Remote     
+public interface CalcBeanRemote {
+	
+	int sum(int a, int b);
+	int sub(int a, int b);
+	int mult(int a, int b);
+
+}
+
---
+++ EjbClientEx/src/com/ejb/calc/HelloWorldSessionBeanRemote.java
@@ -0,0 +1,7 @@
+package com.ejb.calc;
+
+public interface HelloWorldSessionBeanRemote {
+	
+	String sayHello();
+
+}
